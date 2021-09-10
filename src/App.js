83ab298d1d364e6630f5b3c6d614439e97ea7e65
Thyara <thyaranonato@outlook.com>
2021-09-10 20:29:24 -0300

    Adiciona exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dados: '',
+    };
+    this.fetchDogImage = this.fetchDogImage.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDogImage();
+  }
+
+  fetchDogImage() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((result) => this.setState({ dados: result }));
+  }
+
+  render() {
+    const { dados } = this.state;
+    if (dados === '') return 'Loading...';
+    return (
+      <div className="App">
+        <p>Dogs Image</p>
+        <button type="button" onClick={ this.fetchDogImage }>New Dog!</button>
+        <div>
+          <img src={ dados.message } alt="Dog Aleatório" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
